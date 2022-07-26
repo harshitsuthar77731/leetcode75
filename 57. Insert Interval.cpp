62283a176345db4001a64b92fbf77db08ed80839
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-07-26 17:45:35 +0530

    Create 57. Insert Interval.cpp

---
+++ 57. Insert Interval.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& ni) {
+        stack<int>st;
+        in.push_back(ni);
+        sort(in.begin(),in.end());
+        for(int i=0;i<in.size();i++){
+            if(st.empty())
+            {   st.push(in[i][0]);
+                st.push(in[i][1]);
+            }else{
+                int pre = st.top();
+                st.pop();
+                int currf = in[i][0];
+                int currs = in[i][1];
+                
+                if(pre>currs){
+                    st.push(pre);
+                    continue;
+                }
+                else if(pre>=currf)
+                    st.push(currs);
+                else{
+                    st.push(pre);
+                    st.push(currf);
+                    st.push(currs);
+                }
+            }
+            
+        }
+        vector<vector<int>>v;
+        // cout<<st.size();
+            while(st.size()>0){
+                int t= st.top();
+                st.pop();
+                int p = st.top();
+                st.pop();
+                v.push_back({p,t});
+            }
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
